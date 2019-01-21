f474103086efeecefba93df99c8f48d47733c5dd
Jean Farines <moukira45@gmail.com>
2019-01-21 18:18:27 +0100

    Ajout des attaques Cookie et LoveBeam au PlayerController

---
+++ Assets/Scripts/BeamBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamBehaviour : MonoBehaviour {
+
+    private float _birthDate;
+    private float _lifeTime = 0;
+    private float _speed = 0;
+    private float _heading = 0;
+    
+	void Start () {
+        _birthDate = Time.time;
+	}
+	
+    public void setBeamParameters(float heading, float speed, float time)
+    {
+        _heading = heading;
+        _speed = speed;
+        _lifeTime = time;
+    }
+
+	void Update () {
+        if (Time.time > _birthDate + _lifeTime)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Vector2 direction = Quaternion.AngleAxis(_heading, Vector3.forward) * Vector2.right;
+            transform.position = Vector2.MoveTowards(transform.position, (Vector2)transform.position + _speed * Time.deltaTime * direction, _speed * Time.deltaTime);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,23 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Directions { EAST, NORTH, WEST, SOUTH };
+
 public class PlayerController : MonoBehaviour {
 
-    public GameObject _Cookie;
-    public int _heading = 0;
-    public float attackDelay = 0;
+    public GameObject CookiePrefab;
+    public GameObject BeamPrefab;
 
-    private float _attackTime = 0;
+    public Sprite FrontSprite;
+    public Sprite BackSprite;
+    public Sprite LeftSprite;
+    public Sprite RightSprite;
+
+    public float speed;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _renderer;
+    private GameObject _cookie;
+    private GameObject _beam;
+
+    private Directions _heading;
+
+    private bool _attacking;
 
 	// Use this for initialization
 	void Start () {
-		
+        _rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _heading = Directions.SOUTH;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        if (Time.time > _attackTime + attackDelay)
+        ChangeSpriteToMatchDirection();
+
+        if (!_cookie && !_beam)
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
@@ -26,16 +45,91 @@ public class PlayerController : MonoBehaviour {
             }
             else if (Input.GetKeyDown(KeyCode.Z))
             {
-                Debug.Log("non");
+                beamAttack();
+            }
+        }
+        else if (_cookie)
+        {
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                Destroy(_cookie);
             }
         }
 	}
 
+    private void FixedUpdate()
+    {
+        if (!_cookie && !_beam)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        float horizontalOffset = Input.GetAxis("Horizontal");
+        float verticalOffset = Input.GetAxis("Vertical");
+
+        // Translates the player to a new position, at a given speed.
+        Vector2 newPos = new Vector2(transform.position.x + horizontalOffset,
+                                     transform.position.y + verticalOffset)
+                                     * speed;
+        _rb.MovePosition(newPos);
+
+        // Computes the player main direction (North, Sound, East, West)
+        if (Mathf.Abs(horizontalOffset) > Mathf.Abs(verticalOffset))
+        {
+            if (horizontalOffset > 0)
+            {
+                _heading = Directions.EAST;
+            }
+            else
+            {
+                _heading = Directions.WEST;
+            }
+        }
+        else if (Mathf.Abs(horizontalOffset) < Mathf.Abs(verticalOffset))
+        {
+            if (verticalOffset > 0)
+            {
+                _heading = Directions.NORTH;
+            }
+            else
+            {
+                _heading = Directions.SOUTH;
+            }
+        }
+    }
+
+    private void ChangeSpriteToMatchDirection()
+    {
+        if (_heading == Directions.NORTH)
+        {
+            _renderer.sprite = BackSprite;
+        }
+        else if (_heading == Directions.SOUTH)
+        {
+            _renderer.sprite = FrontSprite;
+        }
+        else if (_heading == Directions.EAST)
+        {
+            _renderer.sprite = RightSprite;
+        }
+        else if (_heading == Directions.WEST)
+        {
+            _renderer.sprite = LeftSprite;
+        }
+    }
+
     void cookieAttack()
     {
-        GameObject obj = (GameObject)Instantiate(_Cookie, transform.position + Quaternion.AngleAxis(_heading * 90, Vector3.forward) *  new Vector3(8, 0), Quaternion.identity);
-        obj.GetComponent<CookieBehaviour>().setLifeTime(1.1f * attackDelay);
-        _attackTime = Time.time;
+        _cookie = (GameObject)Instantiate(CookiePrefab, transform.position + Quaternion.AngleAxis((float)_heading * 90f, Vector3.forward) *  new Vector3(8, 0), Quaternion.identity);
+    }
+
+    void beamAttack()
+    {
+        _beam = (GameObject)Instantiate(BeamPrefab, transform.position + Quaternion.AngleAxis((float)_heading * 90f, Vector3.forward) * new Vector3(8, 0), Quaternion.identity);
+        _beam.GetComponent<BeamBehaviour>().setBeamParameters((float)_heading * 90f, 50f, 0.5f);
     }
 
 }
